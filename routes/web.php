4f7357ae7eba7c30b1b1bca787e7e47d80bbd2fe
Admin <mistermustache33@gmail.com>
2022-11-14 14:58:13 +0100

    Made a start on the confidant list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ConfidantController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/list', [ConfidantController::class, 'index'])->name('list');
 
 Auth::routes();
 
