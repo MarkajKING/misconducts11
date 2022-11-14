b10bb3e9cbc3000410cdfae1a71b54afac7d5290
Admin <mistermustache33@gmail.com>
2022-11-14 15:24:59 +0100

    Added confidant list page template with small confidant information. Doesn't have full functionality yet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
     <div id="app">
