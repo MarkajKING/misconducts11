a53ba52a4448fdc957eeef00509abd7f2b5d4e97
Admin <mistermustache33@gmail.com>
2022-11-22 01:13:13 +0100

    tried to connect with database. confused

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,67 +6,49 @@
                 <div class="card">
                     <div class="card-header">
                         <h2>CONFIDANTS</h2>
-                        <div class="card-body">
-                            <br>
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th></th>
-                                        <th>Name</th>
-                                        <th>Age</th>
-                                        <th>Gender</th>
-                                        <th>Specialization</th>
-                                        <th>Availability</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td><img
-                                                src="https://media-exp1.licdn.com/dms/image/D4E03AQFajLztb5d2tA/profile-displayphoto-shrink_800_800/0/1664554842119?e=1674086400&v=beta&t=tFuStHrnYHPY_ukQU2VuB-BI6aNix47HKOUksrE1AoE"
-                                                width="150" height="150" title="Logo of a company"
-                                                alt="Logo of a company"/></td>
-                                        <td>Sasha Salmon</td>
-                                        <td>20</td>
-                                        <td>Male</td>
-                                        <td>Bullying</td>
-                                        <td><i class="glyphicon glyphicon-user" style="font-size:24px;color:green;"></i>
-                                        </td>
-                                        <td>
-                                            <a href="" title="View Confidant">
-                                                <button class="btn btn-info btn-sm"><i class="fa fa-eye"
-                                                                                       aria-hidden="true"></i> Select
-                                                </button>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td><img src="https://wstartupc.com/wp-content/uploads/2021/08/anu-sing.jpg"
-                                                 width="150" height="150" title="Logo of a company"
-                                                 alt="Logo of a company"/></td>
-                                        <td>Sylvia Roosen</td>
-                                        <td>32</td>
-                                        <td>Female</td>
-                                        <td>Sexual Abuse</td>
-                                        <td><i class="glyphicon glyphicon-user" style="font-size:24px;color:red;"></i>
-                                        </td>
-                                        <td>
-                                            <a href="" title="View Confidant">
-                                                <button class="btn btn-info btn-sm"><i class="fa fa-eye"
-                                                                                       aria-hidden="true"></i> Select
-                                                </button>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                    </div>
+                    <div class="card-body">
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>Age</th>
+                                    <th>Gender</th>
+                                    <th>Specialization</th>
+                                    <th>Availability</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($confidants as $confidant)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $confidants->first_name }}</td>
+                                            <td>{{ $confidants->age }}</td>
+                                            <td>{{ $confidants->gender }}</td>
+                                            <td>{{ $confidants->experiences }}</td>
+                                            <td>{{ $confidants->about}}</td>
+                                            <td><i class="glyphicon glyphicon-user" style="font-size:24px;color:green;"></i>
+                                            </td>
+                                            <td>
+                                                <a href="" title="View Confidant">
+                                                    <button class="btn btn-info btn-sm"><i class="fa fa-eye"
+                                                                                           aria-hidden="true"></i> Select
+                                                    </button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 @endsection
